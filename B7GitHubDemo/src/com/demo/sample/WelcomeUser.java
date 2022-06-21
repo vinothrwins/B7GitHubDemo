cc5929739c7d935a6c62ccb866d18efa66c628e8
Vinoth Rathinam <vinothrwins@gmail.com>
2022-06-21 14:55:26 +0530

    New project

---
+++ B7GitHubDemo/src/com/demo/sample/WelcomeUser.java
@@ -0,0 +1,13 @@
+package com.demo.sample;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		
+		
+		// Code added by Vinoth
+		System.out.println("Welcome to Github Demo");
+	
+	}
+
+}
